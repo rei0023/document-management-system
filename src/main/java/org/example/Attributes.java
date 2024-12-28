0bc3949b9376860b3f612a7957ac858a713d72ec
reinaldo <rqmdrno23@gmail.com>
2024-12-28 14:12:37 -0600

    Add ImageImporter class to import images

---
+++ src/main/java/org/example/Attributes.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Attributes {
+    public static final String PATH = "path";
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
+    public static final String TYPE = "image";
+}
