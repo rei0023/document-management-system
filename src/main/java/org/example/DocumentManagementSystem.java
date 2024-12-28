ffc475b806184f2ac9611adab1c2485bc9316d92
reinaldo <rqmdrno23@gmail.com>
2024-12-28 14:22:40 -0600

    Add DocumentManagementSystem class

---
+++ src/main/java/org/example/DocumentManagementSystem.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DocumentManagementSystem {
+    private final Map<String, Importer> extensionToImporter = new HashMap<>();
+    public DocumentManagementSystem() {
+        extensionToImporter.put("letter", new LetterImporter());
+        extensionToImporter.put("report", new ReportImporter());
+        extensionToImporter.put("jpg", new ImageImporter());
+    }
+}
