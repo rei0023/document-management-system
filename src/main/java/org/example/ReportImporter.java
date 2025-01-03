1dff25cee8a0bf5fb1d982b71acc491f76c54e07
reinaldo <rqmdrno23@gmail.com>
2025-01-03 13:56:32 -0600

    Implement class to import report files

--- src/main/java/org/example/ReportImporter.java
+++ src/main/java/org/example/ReportImporter.java
@@ -2,10 +2,24 @@ package org.example;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
+
+import static org.example.Attributes.*;
 
 public class ReportImporter implements Importer {
+    private static final String NAME_PREFIX = "Patient: ";
+    private static final int LINE_BODY = 2;
+
     @Override
     public Document importFile(File file) throws IOException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        final TextFile textFile = new TextFile(file);
+
+        textFile.addLineSuffix(NAME_PREFIX, PATIENT);
+
+        textFile.addLines(LINE_BODY, String::isEmpty, BODY);
+
+        final Map<String, String> attributes = textFile.getAttributes();
+        attributes.put(TYPE, "REPORT");
+        return new Document(attributes);
     }
 }
